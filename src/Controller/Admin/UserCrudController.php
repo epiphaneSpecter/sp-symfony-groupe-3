e8b4d80e77975173611bc680dcbf226c50b9239e
Epiphane AVLAH <epiphanespecter@gmail.com>
2024-07-02 23:45:18 -0400

    add user creation and edition

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,26 +3,74 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if ($entityInstance instanceof User) {
+            $this->encodePassword($entityInstance);
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if ($entityInstance instanceof User) {
+            $this->encodePassword($entityInstance);
+        }
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->onlyWhenCreating(),
+            BooleanField::new('isVerified')
+                ->setFormTypeOption('mapped', false),
+            ChoiceField::new('roles')->setChoices([
+                'Lecteur' => 'ROLE_USER',
+                'Administrateur' => 'ROLE_ADMIN',
+            ])->allowMultipleChoices(),
+
         ];
     }
-    */
+
+    private function encodePassword(User $user): void
+    {
+        if ($user->getPassword() !== null) {
+            $user->setPassword(
+                $this->passwordHasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+        }
+    }
+
 }
